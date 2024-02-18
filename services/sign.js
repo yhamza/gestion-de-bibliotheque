ef3b7bbd962cdff35fdad8773a3af2bba60bbec3
yassine hamza <114316966+yhamza@users.noreply.github.com>
2024-02-18 13:30:08 +0100

    Update sign.js

--- services/sign.js
+++ services/sign.js
@@ -2,12 +2,12 @@ const Admin=require('../models/Admin')
 const jwt=require('jsonwebtoken')
 const dbConnection = require("../config/database");
 const asyncHandler = require('express-async-handler')
-
+const bcrypt=require('bcryptjs')
 exports.signup = asyncHandler(async (req, res) => {
     // 1- Create admin
     const newAdmin = new Admin({
         email: req.body.email,
-        password: req.body.password,
+        password:await bcrypt.hash(req.body.password,12),
     });
     await newAdmin.save()
     console.log("admin created ")
